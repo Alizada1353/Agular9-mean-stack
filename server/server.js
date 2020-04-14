c0f16d0ca11c22759c1b57e94ea1150b3b0ac236
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-14 17:57:07 +0200

    created fronend loging/sigup pages and configured backend first steps

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+require("dotenv").config();
+const cors = require('cors');
+
+/** control cross-origin requests */
+const allowedOrigins = process.env.allowedOrigins.split(',');
+app.use(cors({
+    origin: function(origin, callback) {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'Access is not allowed to this origin!'
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}))
+/** control cross-origin requests */
+
+
+
+app.get('/', (req, res) => res.send('connection successful!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
