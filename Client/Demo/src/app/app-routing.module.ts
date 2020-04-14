60a889abd5199d2f26d09ec03de833b9afffcb81
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-14 15:04:29 +0200

    configured login authentication services

--- Client/Demo/src/app/app-routing.module.ts
+++ Client/Demo/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { AuthGuardService as AuthGuard } from "./services/auth-guard.service";
+
 
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -8,12 +10,19 @@ import { SignupComponent } from './components/signup/signup.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full' },
-  {path: 'login', component: LoginComponent},
-  {path: 'signup', component: SignupComponent },
-  {path: 'home', component: HomeComponent},
-  {path: 'dashboard', component: DashboardComponent },
-  {path: '**', redirectTo: '/login', pathMatch: 'full' }
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "home", 
+    component: HomeComponent, 
+    canActivate: [AuthGuard] 
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: "**", redirectTo: "/login", pathMatch: "full" },
 ];
 
 @NgModule({
