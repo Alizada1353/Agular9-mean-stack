3932430e9d960637eb0318b8491a8109d27afa19
Alizada_Rahim <abdul_rahim.alizada@novartis.com>
2020-04-16 11:48:57 +0200

    removed child folder demo and reinstalled dependencies

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { AuthGuardService as AuthGuard } from "./services/auth-guard.service";
+
+
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+
+
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "home", 
+    component: HomeComponent, 
+    canActivate: [AuthGuard] 
+  },
+  {
+    path: "dashboard",
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: "**", redirectTo: "/login", pathMatch: "full" },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
